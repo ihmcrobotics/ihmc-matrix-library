2d3af385d42d77aa5baab34a1729d7c8b7fdbb56
Sylvain Bertrand <sbertrand@ihmc.us>
2020-10-30 10:00:32 -0500

    Formatted and documented the nullspace calculator.

--- src/main/java/us/ihmc/matrixlib/NativeNullspaceProjector.java
+++ src/main/java/us/ihmc/matrixlib/NativeNullspaceProjector.java
@@ -2,32 +2,51 @@ package us.ihmc.matrixlib;
 
 import us.ihmc.matrixlib.jni.NativeNullspaceProjectorImpl;
 
+/**
+ * {@code NativeNullspaceProjector} can be used to project a first matrix into the nullspace of a
+ * second matrix. The entire operation is performed in C++ to maximize performance.
+ * 
+ * @author Jesper Smith
+ */
 public class NativeNullspaceProjector
 {
    private final NativeNullspaceProjectorImpl impl;
-   
-   public NativeNullspaceProjector(int degreesOfFreedom)
+
+   /**
+    * Creates a new instance of a nullspace projector for a given problem size.
+    * 
+    * @param matrixSize the problem size this calculator can solve.
+    */
+   public NativeNullspaceProjector(int matrixSize)
    {
-      impl = new NativeNullspaceProjectorImpl(degreesOfFreedom);
+      impl = new NativeNullspaceProjectorImpl(matrixSize);
    }
-   
 
    /**
     * Projects the matrix {@code a} onto the null-space of {@code b} and stores the result in {@code c}
     * such that</br>
     * b * c == 0</br>
     * This method uses a damped least square approach causing the null-space to grow gradually.
+    * <p>
+    * The projection is computed as follows:</br>
+    * c = a * &Nu;</br>
+    * &Nu; = I - b<sup>+</sup>b</br>
+    * b<sup>+</sup> = b<sup>T</sup> ( b b<sup>T</sup> - &alpha; I)<sup>-1</sup></br>
+    * where &Nu; is the nullspace projector and B<sup>+</sup> the Moore-Penrose pseudo-inverse of b.
+    * </p>
     * 
-    * @param a     matrix to project
-    * @param b     matrix to compute the null-space of
-    * @param c     where the result is stored (modified)
-    * @param alpha damping value
+    * @param a     matrix to project. The matrix size is n-by-m where n is unconstrained and m is
+    *              constrained to the problem size of this calculator. Not modified.
+    * @param b     matrix to compute the null-space of. The matrix size is p-by-m where p is
+    *              unconstrained and m is constrained to the problem size of this calculator. Not
+    *              modified.
+    * @param c     where the result is stored. The matrix is resized to a n-by-m matrix. Modified.
+    * @param alpha damping value.
     * @throws IllegalArgumentException if the matrix dimensions are incompatible.
     */
    public void project(NativeMatrix a, NativeMatrix b, NativeMatrix c, double alpha)
    {
-
-      if(!impl.projectOnNullSpace(a.impl, b.impl, c.impl, alpha))
+      if (!impl.projectOnNullSpace(a.impl, b.impl, c.impl, alpha))
       {
          throw new IllegalArgumentException("Incompatible Matrix Dimensions.");
       }
