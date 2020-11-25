d1668fb24d66b94f09104377dc314c0c2389e9df
Duncan Calvert <dcalvert@ihmc.us>
2020-11-24 22:47:24 -0600

    :arrow_up: ihmc-commons 0.30.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ mainDependencies {
    api("org.ejml:ejml-core:0.39")
    api("org.ejml:ejml-ddense:0.39")
 
-   api("us.ihmc:ihmc-commons:0.29.0")
+   api("us.ihmc:ihmc-commons:0.30.4")
    api("us.ihmc:euclid:0.15.1")
    api("us.ihmc:ihmc-native-library-loader:1.2.1")
 }
