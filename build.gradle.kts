0625d60dfd230ac33453e5c959238124391e0f06
Jesper Smith <jesper@halodi.com>
2021-02-22 16:35:43 +0100

    Bumped version for testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.18.4-halodi2"
+   version = "0.18.4-halodi3"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-matrix-library"
    openSource = true
 
