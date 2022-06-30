54504313fcf6d7c82832d658bf2f3362e713dab5
Sylvain Bertrand <sbertrand@ihmc.org>
2022-06-30 15:24:53 -0500

    :arrow_up: euclid 0.18.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,11 +19,11 @@ mainDependencies {
    api("org.ejml:ejml-ddense:0.39")
 
    api("us.ihmc:ihmc-commons:0.31.0")
-   api("us.ihmc:euclid:0.17.2")
+   api("us.ihmc:euclid:0.18.1")
    api("us.ihmc:ihmc-native-library-loader:1.3.1")
 }
 
 testDependencies {
-   api("us.ihmc:euclid-frame:0.17.2")
+   api("us.ihmc:euclid-frame:0.18.1")
    api("org.ejml:ejml-simple:0.39")
 }
