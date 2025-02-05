fedf4a55ad565fe8c1dc9dd3ebe48a166bf3a2bd
ds58 <30220598+ds58@users.noreply.github.com>
2025-02-05 13:20:49 -0600

    :arrow_up: ihmc-commons 0.35.1 (#4)

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ mainDependencies {
    api("org.ejml:ejml-core:0.39")
    api("org.ejml:ejml-ddense:0.39")
 
-   api("us.ihmc:ihmc-commons:0.34.0")
+   api("us.ihmc:ihmc-commons:0.35.1")
    api("us.ihmc:euclid:0.22.2")
    api("us.ihmc:ihmc-native-library-loader:2.0.3")
    api("net.sf.trove4j:trove4j:3.0.3")
