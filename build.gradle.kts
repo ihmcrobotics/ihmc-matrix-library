a236fa05b91cf13b51f600899763acb39d7ede30
Dexton Anderson <danderson@ihmc.org>
2025-02-06 13:24:34 -0600

    :arrow_up: euclid 0.22.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,12 +17,12 @@ mainDependencies {
    api("org.ejml:ejml-ddense:0.39")
 
    api("us.ihmc:ihmc-commons:0.35.1")
-   api("us.ihmc:euclid:0.22.2")
+   api("us.ihmc:euclid:0.22.3")
    api("us.ihmc:ihmc-native-library-loader:2.0.3")
    api("net.sf.trove4j:trove4j:3.0.3")
 }
 
 testDependencies {
-   api("us.ihmc:euclid-frame:0.22.2")
+   api("us.ihmc:euclid-frame:0.22.3")
    api("org.ejml:ejml-simple:0.39")
 }
